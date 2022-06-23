ad208dfe4c57a05d13f8a4d717505cd381fed1b4
Ivan Skvortsov <pprofcheg@gmail.com>
2022-06-23 16:15:07 +0300

    modified constants

--- src/constants.py
+++ src/constants.py
@@ -6,6 +6,11 @@ MAIN_DOC_URL = 'https://docs.python.org/3/'
 MAIN_PEP_URL = 'https://peps.python.org/'
 
 BASE_DIR = Path(__file__).parent
+LOG_DIR = BASE_DIR / 'logs'
+LOG_FILE = LOG_DIR / 'parser.log'
+
+LOG_FORMAT = '%(asctime)s - [%(levelname)s] - %(message)s'
+LOG_DT_FORMAT = '%d.%m.%Y %H:%M:%S'
 
 EXPECTED_STATUS = {
     'A': ['Active', 'Accepted'],
